8808d8fb2d4790326725d6eb156a8e08263ad8b9
Sridhar Tiwari <tiwari4002@outlook.com>
2025-10-07 22:25:43 -0400

    Complete Stage 1 - Remove unused footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,26 +14,6 @@ function Footer({ className = "" }) {
           <p className="footer__copyright">
             &copy; {currentYear} Sridhar Tiwari. All rights reserved.
           </p>
-
-          <nav className="footer__nav">
-            <ul className="footer__nav-list">
-              <li className="footer__nav-item">
-                <a href="/privacy" className="footer__nav-link">
-                  Privacy
-                </a>
-              </li>
-              <li className="footer__nav-item">
-                <a href="/terms" className="footer__nav-link">
-                  Terms
-                </a>
-              </li>
-              <li className="footer__nav-item">
-                <a href="/contact" className="footer__nav-link">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </nav>
         </div>
       </div>
     </footer>
